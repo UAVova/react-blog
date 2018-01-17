f47a96d8b002d93a062897a2b7c6a1b5b17ff3e1
UAVova <dev.volodymyr@gmail.com>
2018-01-17 02:33:57 +0200

    Added react router v4 with basic path

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { 
+  BrowserRouter as Router, 
+  Route
+} from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
+import Switch from 'react-router-dom/Switch';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
