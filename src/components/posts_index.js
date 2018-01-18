abded4eb075d8a5a97a6899cce88f32f240df0d9
UAVova <dev.volodymyr@gmail.com>
2018-01-18 17:30:17 +0200

    Added additional libs, posts index component skeleton

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        Posts List
+      </div>
+    );
+ }
+}
+
+export default PostsIndex;
