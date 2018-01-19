318a26c752242758a3b547322a1c065775c9ba02
UAVova <dev.volodymyr@gmail.com>
2018-01-19 02:06:16 +0200

    Added  fetch action on the posts component, deleted dummy component

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import PostsIndex from './posts_index';
 
-const Greeting = () => {
-  return <div>Greet you!</div>
-}
-
 export default class App extends Component {
   render() {
     const { match } = this.props;
@@ -13,7 +9,6 @@ export default class App extends Component {
     return (
       <div>
         <Route exact path={match.url} component={PostsIndex} />
-        <Route path={`${match.url}greet`} render={() => <div>Greet you!</div>} />
       </div>
     );
   }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions';
 
+@connect(null, { fetchPosts })
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
