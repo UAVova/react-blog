7a61f1e2e3e56957d27f1dd580ce8cf0ea9d31cc
UAVova <dev.volodymyr@gmail.com>
2018-01-29 00:04:13 +0200

    Implemented posts deleting

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@ const API_KEY = '?key=somerandomstring';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -31,6 +32,15 @@ export function fetchPost(id) {
 
   return {
     type: FETCH_POST,
+    payload: request
+  }
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
     payload: request
   }
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 import Link from 'react-router-dom/Link';
 
-@connect((state) => ({post: state.posts.post}), {fetchPost})
+@connect((state) => ({post: state.posts.post}), {fetchPost, deletePost})
 class PostsShow extends Component {
   componentDidMount() {
     this.props.fetchPost(this.props.match.params.id);
+  }
+
+  onPostDelete () {
+    this.props.deletePost(this.props.post.id)
+      .then(() => this.props.history.push('/'));
   }
 
   render() {
@@ -19,6 +24,11 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to='/'>Back</Link>
+        <button 
+          onClick={() => this.onPostDelete()} 
+          className='btn btn-danger pull-xs-right'>
+          Delete post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
